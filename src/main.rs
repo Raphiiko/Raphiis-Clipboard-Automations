49f5e362e72484326fec421a03481cef813c2845
Raphiiko <iam@raphii.co>
2024-06-12 12:06:19 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use clipboard_master::{CallbackResult, ClipboardHandler, Master};
+use clipboard_win::{formats, get_clipboard, set_clipboard};
+use regex::Regex;
+
+const REPLACEMENTS: &[(&str, &str)] = &[
+    (
+        r"\b(http|https)://x.com/([A-Za-z0-9_]{4, 15})/status",
+        r"https://fixvx.com/$2/status",
+    ),
+    (
+        r"\b(http|https)://twitter.com/([A-Za-z0-9_]{4, 15})/status",
+        r"https://vxtwitter.com/$2/status",
+    ),
+    (
+        r"\b(http|https)://(www\.)tiktok.com/@([A-Za-z0-9_.]{4, 15})/video",
+        r"https://www.vxtiktok.com/@$3/video",
+    ),
+];
+
+fn main() {
+    let result = Master::new(Handler).run();
+    if let Err(e) = result {
+        eprintln!("Couldn't listen for clipboard changes: {}", e)
+    }
+}
+
+struct Handler;
+
+impl ClipboardHandler for Handler {
+    fn on_clipboard_change(&mut self) -> CallbackResult {
+        // Get clipboard content
+        let content: String = match get_clipboard(formats::Unicode) {
+            Ok(clipboard) => clipboard,
+            Err(_) => {
+                return CallbackResult::Next;
+            }
+        };
+
+        // Attempt to modify content
+        let modified = patch_clipboard_content(content.clone());
+
+        // Set the clipboard if changes were made
+        if modified != content {
+            match set_clipboard(formats::Unicode, modified.clone()) {
+                Ok(_) => {
+                    println!(
+                        "Clipboard content updated.\nPrevious Content: {}\nNew Content: {}",
+                        content, modified
+                    );
+                }
+                Err(e) => {
+                    eprintln!("Couldn't update clipboard content: {}", e);
+                }
+            }
+        }
+
+        CallbackResult::Next
+    }
+}
+
+fn patch_clipboard_content(content: String) -> String {
+    let mut content = content;
+    for (pattern, replacement) in REPLACEMENTS.iter() {
+        content = Regex::new(pattern)
+            .unwrap()
+            .replace_all(&content, *replacement)
+            .to_string();
+    }
+    content
+}
